a3028d9911c116e9e5446a19458d9c0180b92032
Ernest Bruno <ernest.bruno94@yahoo.com>
2018-09-04 12:32:01 -0700

    Add require statement for handlebars, and use res.render( ) to render template .hbs file, passing in JSON data as second argument to render, to be used in .hbs file

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+// SET Express related configurations. Pass in key-value pairs.
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 // Register a handler.
@@ -13,17 +16,27 @@ app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
 
   // Returns a json data
-  res.send({
-    name: 'Ernest',
-    likes: [
-      'Working out',
-      'Coding'
-    ]
-  });
+  // res.send({
+  //   name: 'Ernest',
+  //   likes: [
+  //     'Working out',
+  //     'Coding'
+  //   ]
+  // });
+
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    pageBody: 'Welcome to the Home Page'
+  })
 });
 
 app.get('/about', (req, res) => {
-  res.send('About Page');
+  // res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 // CHALLENGE
