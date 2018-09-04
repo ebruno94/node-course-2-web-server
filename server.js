b6d2abbc3d655ba0e3a230df2a1f2420fd3e493e
Ernest Bruno <ernest.bruno94@yahoo.com>
2018-09-04 12:05:13 -0700

    Create express app with a few paths

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+var app = express();
+
+// Register a handler.
+// Sets up a handler for HTTP get request to the given URL (this case is our root).
+// Sends back a function (req, res).
+// Request stores info about the request (Headers, body info, method made with request to the path);
+// Response has a bunch of methods that allows us to respond HTTP request in any way desired.
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express!</h1>');
+
+  // Returns a json data
+  res.send({
+    name: 'Ernest',
+    likes: [
+      'Working out',
+      'Coding'
+    ]
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+// CHALLENGE
+// Create a route to /bad, send back JSON data with errorMessage.
+
+app.get('/bad', (req, res) => {
+  res.send({
+    statusCode: 'BAD',
+    errorMessage: 'Bad Request'
+  });
+});
+
+// Binds application port to our machine.
+app.listen(3000);
