64c0f29ac7c105397e795f437c2ad5979cd40f4a
Ernest Bruno <ernest.bruno94@yahoo.com>
2018-09-16 10:41:39 -0700

    Add logic using express in order to display webpages and data

--- server.js
+++ server.js
@@ -1,11 +1,50 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+// registerPartials takes directory we want to use for handlebar partial files.
+hbs.registerPartials(__dirname + '/views/partials');
+
 // SET Express related configurations. Pass in key-value pairs.
 app.set('view engine', 'hbs');
+
+// Register middleware by using app.use. Takes a function.
+// next exists so you can tell express when middleware function is done. Useful cause you can have as much middlewares as you like registered in the express app.
+// req object contains request info (http method, path, etc.)
+app.use((req, res, next) => {
+  // let's make a logger with a timestamp.
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+
+  // appendFile lets us add into a file.
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+
+  // end current middleware, move on to next one.
+  next();
+});
+
+// CHALLENGE Maintenance.hbs
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs')
+// });
+
 app.use(express.static(__dirname + '/public'));
+
+// Register helper, (name of function in quotes, function);
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 
 // Register a handler.
 // Sets up a handler for HTTP get request to the given URL (this case is our root).
@@ -23,11 +62,9 @@ app.get('/', (req, res) => {
   //     'Coding'
   //   ]
   // });
-
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    currentYear: new Date().getFullYear(),
-    pageBody: 'Welcome to the Home Page'
+    welcomeMessage: 'Welcome to the Home Page'
   })
 });
 
@@ -35,7 +72,6 @@ app.get('/about', (req, res) => {
   // res.send('About Page');
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
   });
 });
 
@@ -48,6 +84,8 @@ app.get('/bad', (req, res) => {
     errorMessage: 'Bad Request'
   });
 });
+
+
 
 // Binds application port to our machine.
 // takes an optional second argument, a function.
